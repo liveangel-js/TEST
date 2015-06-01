0ae28f718146aeb758c84df2fbc92a3eb9378b59
gstsqawhh <xlb325star@163.com>
2015-06-01 14:47:32 +0800

    place order

--- app/src/main/java/com/example/liveangel/test/Main.java
+++ app/src/main/java/com/example/liveangel/test/Main.java
@@ -1,17 +1,33 @@
 package com.example.liveangel.test;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.place.order.ConfirmOrderActivity;
 
 
 public class Main extends ActionBarActivity {
-
+    private Button orderButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        orderButton = (Button) findViewById(R.id.orderButton);
+        orderButton.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Main.this, ConfirmOrderActivity.class);
+                startActivity(intent);
+                Main.this.finish();
+            }
+        });
     }
 
 
